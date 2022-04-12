5710c586cee87f23c4fa59cfad0fbe3b6741a182
Tarcizo Neto <tarcizon12@gmail.com>
2022-04-12 16:31:12 -0300

    initializing data modeling

---
+++ app/src/main/java/com/example/projetosistemasolar/moldsData/DataSourcePlanets.kt
@@ -0,0 +1,16 @@
+package com.example.projetosistemasolar.moldsData
+
+class DataSourcePlanets {
+
+    companion object{
+        fun createDataSet(): ArrayList<Planets>{
+            val planetsList = ArrayList<Planets>()
+
+
+            
+            return planetsList
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/projetosistemasolar/moldsData/Planets.kt
@@ -0,0 +1,12 @@
+package com.example.projetosistemasolar.moldsData
+
+data class Planets(
+    var namePlanet: String,
+    var text: String,
+    var urlArticle: String,
+    var urlPicture: String
+
+)
+
+
+//Modelagem de dados
