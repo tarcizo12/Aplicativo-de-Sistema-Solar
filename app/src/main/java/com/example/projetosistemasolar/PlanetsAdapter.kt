157061ebe17350f38a494e865b6bb9a465a43981
Tarcizo Neto <tarcizon12@gmail.com>
2022-04-12 21:38:59 -0300

    returning size list

--- app/src/main/java/com/example/projetosistemasolar/PlanetsAdapter.kt
+++ app/src/main/java/com/example/projetosistemasolar/PlanetsAdapter.kt
@@ -6,15 +6,16 @@ import com.example.projetosistemasolar.models.Planets
 
 class PlanetsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
     private var planets :  List<Planets> = ArrayList()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return planets.size
     }
 }
