48b44f17c9479e381b0c50b4d6f32a479c72dfe7
Tarcizo Neto <tarcizon12@gmail.com>
2022-04-13 17:38:20 -0300

    ending ViewHolder

--- app/src/main/java/com/example/projetosistemasolar/PlanetsAdapter.kt
+++ app/src/main/java/com/example/projetosistemasolar/PlanetsAdapter.kt
@@ -1,14 +1,20 @@
 package com.example.projetosistemasolar
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projetosistemasolar.models.Planets
+import kotlinx.android.synthetic.main.planets_itens.view.*
 
 class PlanetsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
     private var planets :  List<Planets> = ArrayList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        
+
+            return PlanetViewHolder(
+                LayoutInflater.from(parent.context).inflate(R.layout.planets_itens,parent,false)
+            )
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
@@ -18,4 +24,23 @@ class PlanetsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
     override fun getItemCount(): Int {
         return planets.size
     }
+
+    class PlanetViewHolder constructor (
+        planetView : View
+            ): RecyclerView.ViewHolder(planetView){
+                private val namePlanet = planetView.title
+                private val descíptionOfPlanet = planetView.description
+                private val picturePlanet = planetView.thumbnail
+
+
+            fun bind (planet : Planets) {
+
+                    namePlanet.text = planet.namePlanet
+                    descíptionOfPlanet.text = planet.textAbout
+
+
+                }
+
+
+            }
 }
--- app/src/main/java/com/example/projetosistemasolar/models/Planets.kt
+++ app/src/main/java/com/example/projetosistemasolar/models/Planets.kt
@@ -2,7 +2,7 @@ package com.example.projetosistemasolar.models
 
 data class Planets(
     var namePlanet: String,
-    var text: String,
+    var textAbout: String,
     var urlArticle: String,
     var urlPicture: String
 
