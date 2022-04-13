60da72a8acd3b0d4de6d78d0eda75d4d9e34e553
Tarcizo Neto <tarcizon12@gmail.com>
2022-04-12 21:37:24 -0300

    initializing adapter

---
+++ app/src/main/java/com/example/projetosistemasolar/PlanetsAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.projetosistemasolar
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.projetosistemasolar.models.Planets
+
+class PlanetsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    private var planets :  List<Planets> = ArrayList()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
